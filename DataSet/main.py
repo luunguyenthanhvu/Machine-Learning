ca1519d8d66b07cf43e0df8f11840cecd61d1ddc
luunguyenthanhvu <21130612@st.hcmuaf.edu.vn>
2024-09-04 03:40:09 +0700

    update source

---
+++ DataSet/main.py
@@ -0,0 +1,25 @@
+from  sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+import numpy as np
+iris_dataset = load_iris()
+'''
+
+# in data
+print(iris_dataset.data)
+
+# in ra nhãn
+print(iris_dataset.target)
+
+# in ra số dữ liệu
+print(len(iris_dataset.target))
+'''
+
+# Hàm trả về 1 danh sách
+x_train, x_test, y_train, y_test = train_test_split(iris_dataset.data, iris_dataset.target, random_state=0)
+
+model = DecisionTreeClassifier()
+mymodel = model.fit(x_train, y_train)
+
+X_New = np.array([[6.0 , 3.23, 4.6, 2.5]])
+print(mymodel.predict(X_New))
